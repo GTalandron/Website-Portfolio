71839b85382248e731a5a3e64f4a4de04db3108e
Gil Talandron <talandrongil@gmail.com>
2025-05-04 11:56:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".input", {
+    strings: ["Full Stack Developer", "Computer Engineering Student", "1st year college student from NCST"],
+    typedSpeed: -100,
+    backspeed: -100,
+    loop: true
+})
+
+
+
